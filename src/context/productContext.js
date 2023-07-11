8194f590c90c786c442bd551d2eb315d23c7776c
asrinutku <57988026+asrinutku@users.noreply.github.com>
2023-07-11 22:40:07 +0300

    fetch data from db, shop page update

--- src/context/productContext.js
+++ src/context/productContext.js
@@ -1,11 +1,23 @@
-import { createContext, useMemo, useState } from "react";
+import { createContext, useEffect, useMemo, useState } from "react";
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const ProductContext = createContext({
-  products: [],
+  products: {},
 });
 
 const ProductContextProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState({});
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const data = await getCategoriesAndDocuments();
+      setProducts(data);
+      console.log(data);
+    };
+
+    getCategoriesMap();
+  }, []);
 
   return (
     <ProductContext.Provider
--- src/routes/shop/shop.js
+++ src/routes/shop/shop.js
@@ -1,5 +1,6 @@
 import "./shop.scss";
 
+import { Fragment } from "react/cjs/react.production.min";
 import ProductCard from "../../components/product-card/product-card";
 import { ProductContext } from "../../context/productContext";
 import { useContext } from "react";
@@ -8,11 +9,18 @@ const Shop = () => {
   const { products } = useContext(ProductContext);
 
   return (
-    <div className="shop-container">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+    <Fragment>
+      {Object.keys(products).map((title) => (
+        <Fragment key={title}>
+          <h2>{title}</h2>
+          <div className="shop-container">
+            {products[title].map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Fragment>
       ))}
-    </div>
+    </Fragment>
   );
 };
 
